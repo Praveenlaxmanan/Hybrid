576b911786d383a7abb5e4d45ddd728e74fe2dcd
Praveen Lakshmanan <Praveenlaxmanan@users.noreply.github.com>
2018-03-22 01:42:24 +0530

    Update TimeTaken.java

--- src/com/hybrid/framework/reports/TimeTaken.java
+++ src/com/hybrid/framework/reports/TimeTaken.java
@@ -499,3 +499,351 @@ ls -lrt | xargs rm -rf
 /**************************
 
 SELECT name, MAX(salary) as salary FROM employee select MAX(FaveValue) as FaveValue from tablename
+
+
+Read Excel:
+
+String path = "Input/Selenium.xlsx";
+        FileInputStream fs = null;
+        XSSFWorkbook wb = null;
+        XSSFSheet sheet = null;
+        XSSFRow row = null;
+        XSSFCell cell = null;
+        
+        try{
+            
+            fs = new FileInputStream(new File(path));
+            wb = new XSSFWorkbook(fs);
+            sheet = wb.getSheet("Sheet1");
+            
+            int rowCount = sheet.getLastRowNum();
+            
+            for(int i = 0; i < rowCount; i++){
+                
+                row = sheet.getRow(0);
+                int columnCount = row.getLastCellNum();
+                
+                for(int j = 0; j < columnCount; j++){
+                    
+                    cell = row.getCell(j);
+                    String value = cell.getRichStringCellValue().getString();
+                    if(value.equalsIgnoreCase(columnName)){
+                        row = sheet.getRow(i);
+                        cell = row.getCell(j);
+                        System.out.println(cell.getStringCellValue());
+                    }
+                }
+            }
+            
+        }catch(Exception e){
+            
+            e.printStackTrace();
+            
+        }
+        
+Write Excel:
+
+try{
+        
+        String excelFileName = "Input/Write.xlsx";
+        
+        String colAutoScript = "Automation_Script";
+        String colStatus = "Status";
+        
+        FileInputStream fs = new FileInputStream(new File(excelFileName));
+        
+        XSSFWorkbook wb = new XSSFWorkbook(fs);
+        XSSFSheet sheet = wb.createSheet("Sheet2");
+        XSSFRow rowHead = sheet.createRow(0);
+        rowHead.createCell(0).setCellValue(colAutoScript);
+        rowHead.createCell(1).setCellValue(colStatus);
+        
+        XSSFRow row = sheet.createRow(1);
+        row.createCell(0).setCellValue("Test");
+        
+        
+        FileOutputStream fos = new FileOutputStream(excelFileName);
+        wb.write(fos);
+        fos.close();
+        
+        }catch(Exception e){
+            
+            e.printStackTrace();
+            
+        }
+
+try{
+            
+            fs = new FileInputStream(path);
+            workbook = new XSSFWorkbook(fs);
+            sheet = workbook.getSheet("Sheet1");
+            
+            String[] values = {"one", "two", "three", "four", "five"};
+            for(int rowItr=0; rowItr < 1; rowItr ++){
+                XSSFRow row = sheet.createRow(rowItr);
+                for(int i = 0; i < values.length; i++){
+                    row.createCell(i).setCellValue(values[i]);
+                }
+            }
+            //Closing the Workbook
+            FileOutputStream outputFile = new FileOutputStream(new File(path));
+            workbook.write(outputFile);
+            outputFile.close();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+		
+		
+		
+Edge Browser:
+System.setProperty("webdriver.edge.driver", driverPath+"MicrosoftWebDriver.exe");
+driver = new EdgeDriver();
+Chrome browser:
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--no-sandbox");		
+
+
+
+Check Browser Capabilities:
+
+public static void checkInIEBrowser(){
+
+        try{
+
+            
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability("ignoreZoomSetting", true);
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            
+            System.setProperty("webdriver.ie.driver", "Input/IEDriverServer.exe");
+            driver = new InternetExplorerDriver(capabilities);
+            
+//            System.setProperty("webdriver.gecko.driver", "Input/geckodriver.exe");
+//            driver = new FirefoxDriver();
+            
+//            System.setProperty("webdriver.chrome.driver", "Input/chromedriver.exe");
+//            driver = new ChromeDriver();
+            
+            driver.get("https://www.softwaretestingmaterial.com");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            Thread.sleep(10000);
+            getScreenshot("Input/normalScreenshot.png");
+            getVerticalScreenshot("Input/normalVerticalScreenshot.png");
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+
+        driver.quit();
+
+    }
+
+    getScreenshot:
+	
+    public static void getScreenshot(String path){
+        
+        try{
+        
+        File scr = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scr, new File(path));
+        
+        }catch(Exception e){
+            
+            e.printStackTrace();
+        }
+    }
+    
+	getVerticalScreenshot:
+	
+    public static void getVerticalScreenshot(String path){
+        
+        try{
+        
+        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+        ImageIO.write(screenshot.getImage(), "PNG", new File(path));
+        
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+	
+	//ASCII values  of alphabets: A – Z = 65 to 90, a – z = 97 to 122
+    public static void upperLowerCase(){
+        
+        String str = "AutoProcess";
+        
+        for(int i =0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(c >= 'a' && c <= 'z' ){
+                c = (char) (c -32);
+                System.out.println("Uppercase :"+c);
+            }else if(c >= 'A' && c <= 'Z'){
+                c = (char) (c + 32);
+                System.out.println("Lowercase :"+c);
+            }
+        }
+    }
+
+	dateDifference:
+	
+public static void dateDifference(){
+        
+        
+        LocalDate fromDate = LocalDate.of(2018, Month.MARCH, 21);
+        LocalDate toDate = LocalDate.of(2018, Month.MAY, 23);
+        
+        long difference = ChronoUnit.DAYS.between(fromDate, toDate);
+        System.out.println(difference);
+        
+    }
+
+	
+	childWindow:
+	
+	public static WebDriver childWindow(String pageTitle){
+        
+        for(String id : driver.getWindowHandles()){
+            driver.switchTo().window(id);
+            if(driver.getTitle().contains(pageTitle)){
+                return driver.switchTo().window(id);
+            }
+        }
+        return null;
+    }
+    
+    public static WebDriver parentWindow(){
+        
+        for(String id : driver.getWindowHandles()){
+            return driver.switchTo().window(id);
+        }
+        
+        return null;
+    }
+    
+    
+    switchTabs:
+	
+    public static void switchTabs(){
+        
+        launchBrowser("http://toolsqa.com/automation-practice-switch-windows/");
+        waitUntilExist("//button[@onclick='newBrwTab()']");
+        driver.findElement(By.xpath("//button[@onclick='newBrwTab()']")).click();
+        
+        List<String> ls = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(ls.get(1));
+        
+        waitUntilExist("(//span[@class='menu-text' and contains(text(), 'HOME')])[1]");
+        System.out.println(driver.getTitle());
+        
+        driver.switchTo().window(ls.get(0));
+        waitUntilExist("//button[@onclick='newBrwTab()']");
+        System.out.println(driver.getTitle());
+        
+        driver.quit();
+        
+        Actions actions = new Actions(driver);
+        WebElement element = null;
+        actions.contextClick(element).click().build().perform();
+        
+    }
+	
+	WebTables:
+	
+	public class WebTables {
+
+    public static WebDriver driver;
+    public static String headerColVal =  null;
+    public static int i = 0;
+    public static List<String> ls = new ArrayList<String>();
+    public static List<String> lsSecRow = new ArrayList<String>();
+    public static List<String> lsActualVal = new ArrayList<String>();
+    
+    public static void getValues(){
+    
+        try{
+        
+        String primaryHeaderColName = "Country";
+        String primaryRowName = "Taipei 101";
+        
+        String secRow = "Taiwan|Taipei|509m|2004|3|details";
+        
+        String[] secRowVal = secRow.split("\\|");
+        
+        for(int secRowValue = 0; secRowValue < secRowVal.length; secRowValue++){
+            
+            lsSecRow.addAll(Arrays.asList(secRowVal[secRowValue]));
+            
+        }
+        
+            System.setProperty("webdriver.chrome.driver", "Input/chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get("http://toolsqa.com/automation-practice-table");
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='content']/table/thead/tr/th")));
+
+            List<WebElement> headerCol = driver.findElements(By.xpath("//div[@id='content']/table/thead/tr/th"));    
+
+            for(i = 1; i < headerCol.size(); i++){
+
+                headerColVal = driver.findElement(By.xpath("//div[@id='content']/table/thead/tr/th["+i+"]")).getText();
+                ls.add(headerColVal);
+
+            }
+
+            for(int ii = 0; ii < ls.size(); ii++){
+                String vals = ls.get(ii);
+                if(vals.equalsIgnoreCase(primaryHeaderColName)){
+
+                    //Get the primary rows names
+                    List<WebElement> primaryHeaderRows = driver.findElements(By.xpath("//div[@id='content']/table/tbody/tr/th"));
+                    //Get primary row size
+                    System.out.println("primaryHeaderRows :"+primaryHeaderRows.size());
+                    //Looping through to get the rows
+                    for(int priRowNames = 1; priRowNames <= primaryHeaderRows.size(); priRowNames++){
+
+                        String strPrimaryRowsNames =  driver.findElement(By.xpath("//div[@id='content']/table/tbody/tr["+priRowNames+"]/th")).getText();
+
+                        if(strPrimaryRowsNames.equalsIgnoreCase(primaryRowName)){
+                            for(int getCol = 1; getCol < headerCol.size(); getCol++){
+                                //Get the table data
+                                String getColData = driver.findElement(By.xpath("//div[@id='content']/table/tbody/tr["+priRowNames+"]/td["+getCol+"]")).getText();
+//                                System.out.println("Column Values :"+getColData);
+                                lsActualVal.add(getColData);
+                                
+                            }
+                        }
+                    }    
+                }
+            }
+
+            
+            for(int x = 0; x < lsSecRow.size(); x++){
+                
+                
+                for(int y = 0; y < lsActualVal.size(); y++){
+                    
+                    if(lsSecRow.get(x).equalsIgnoreCase(lsActualVal.get(y))){
+                        
+                        System.out.println("Actual value is '"+lsActualVal.get(y)+"' matched with the expected value '"+lsSecRow.get(x)+"' ");
+                        
+                    }
+                }
+            }
+            
+            
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+    }
+
